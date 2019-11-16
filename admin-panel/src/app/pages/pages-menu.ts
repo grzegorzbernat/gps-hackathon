b4b23ab3b885cf20144f2719fe50aca694d9d4ed
Greggy <g.bernat@greggy.pl>
2019-11-16 14:18:33 +0100

    Admin panel customization

--- admin-panel/src/app/pages/pages-menu.ts
+++ admin-panel/src/app/pages/pages-menu.ts
@@ -2,7 +2,7 @@ import { NbMenuItem } from '@nebular/theme';
 
 export const MENU_ITEMS: NbMenuItem[] = [
   {
-    title: 'Dashboard',
+    title: 'Strona główna',
     icon: 'home-outline',
     link: '/pages/dashboard',
     home: true,
